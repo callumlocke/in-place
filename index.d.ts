bfbb1863f07c93f6a47c9658a004353b09ed1d1e
Kostas Loupasakis <loupax@gmail.com>
2017-05-05 23:03:56 +0300

    Added types to all the array typehints

--- index.d.ts
+++ index.d.ts
@@ -2,17 +2,17 @@ declare module "in-place" {
     let inPlace: InPlace;
 
     class InPlace {
-        public map(array: Array, callback: (item: any, index: number) => void, thisArg?: any): Array;
+        public map(array: Array<any>, callback: (item: any, index: number) => void, thisArg?: any): Array<any>;
 
-        public filter(array: Array, callback: (item: any, index: number) => boolean, thisArg?: any): Array;
+        public filter(array: Array<any>, callback: (item: any, index: number) => boolean, thisArg?: any): Array<any>;
 
-        public concat(array: Array): Array;
+        public concat(array: Array<any>): Array<any>;
 
-        public deleteIndex(array: Array, index: number): Array;
+        public deleteIndex(array: Array<any>, index: number): Array<any>;
 
-        public dedupe(array: Array): Array;
+        public dedupe(array: Array<any>): Array<any>;
 
-        public dedupeSorted(array: Array): Array;
+        public dedupeSorted(array: Array<any>): Array<any>;
     }
 
     export default inPlace;
